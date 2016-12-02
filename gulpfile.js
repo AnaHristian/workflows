4c08e84f8aa3a030116d5e1e61b6f51f65660957
Ana Hristian <Ana Hristian>
2016-12-02 14:46:36 +0200

    minify html

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 // gulp.task('log', function () {
@@ -73,7 +74,7 @@ var gulp = require('gulp'),
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
   });
 
@@ -85,7 +86,9 @@ var gulp = require('gulp'),
   });
 
   gulp.task('html', function() {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+      .pipe(gulpif( env === 'production', minifyHTML() ))
+      .pipe(gulpif( env === 'production', gulp.dest( outputDir ) ))
       .pipe(connect.reload())
   });
 
